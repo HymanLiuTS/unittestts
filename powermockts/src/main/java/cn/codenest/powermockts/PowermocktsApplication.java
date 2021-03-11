119ca7d689398b0050c04821133781fe07ae96d4
HymanLiuTS <879651072@qq.com>
2021-03-11 18:46:35 +0800

    初次提交诚程序

---
+++ powermockts/src/main/java/cn/codenest/powermockts/PowermocktsApplication.java
@@ -0,0 +1,13 @@
+package cn.codenest.powermockts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowermocktsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowermocktsApplication.class, args);
+    }
+
+}
---
+++ powermockts/src/test/java/cn/codenest/powermockts/PowermocktsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codenest.powermockts;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowermocktsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
